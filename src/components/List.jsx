79a1304f24bc5ffd7afeb9e1d2b92fd953efcc50
aswazone <aswamanu143@gmail.com>
2025-02-21 22:16:01 +0530

    feat: useState item rendering with immutable arr !

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -28,34 +28,41 @@ const List = () => {
   ];
 
 
-  const [items,setItems] = useState(arr)
+//   const [items,setItems] = useState(arr)
+  const [value,setValue] = useState('All')
 
   const detectChange = (e) =>{
-    console.log(e.target.value)
+    
+    // console.log(e.target.value)
     const value = e.target.value
-
-    const newList = arr.filter(data => {
-        if(value === 'All') {
-            return true;
-        }
-        if(value === 'Active'){
-           return data.isActive === true;
-        }
-        if(value === 'Inactive'){
-           return data.isActive === false;
-        }
-        return false;
-    })
-
-    console.log(newList)
-    setItems(newList);
+    setValue(value);
 
   }
+
+  //more convenient way of code writing
+//   code is the better approach because: ✅ It keeps the original list (arr) immutable.
+// ✅ Avoids unnecessary state updates, making it more performant.
+// ✅ Ensures that filtering happens only when rendering, without modifying the original data.
+// ✅ Less complexity and easier to debug.
+
+  const newList = arr.filter(data => {
+    if(value === 'All') {
+        return true;
+    }
+    if(value === 'Active'){
+       return data.isActive === true;
+    }
+    if(value === 'Inactive'){
+       return data.isActive === false;
+    }
+    return false;
+})
+
 
   return (
     <>
     <Tools detectChange={detectChange}>
-      {items.map((item) => {
+      {newList.map((item) => {
        return(
         <div key={item.title} className="app-list bg-light">
           <ListItem title={item.title} desc={item.desc} isActive={item.isActive} />
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -2,13 +2,14 @@ import Label from "./Label";
 
 
 
-const ListItem = ({title,desc,isActive}) => {
+const ListItem = ({title,desc,isActive, onDelete}) => {
 
   return (
     <>
       <div className="list-item">
-        <div className="list-title">
+        <div className="list-title d-flex justify-content-between">
           <h4 className="fw-bold">{title}</h4>
+          <button onClick={()=>onDelete(title)} className="btn btn-sm btn-warning ">Delete</button>
         </div>
         <div className="list-desc">{desc}</div>
         <div className="list-label d-flex gap-2 py-2">
